ecb94dc037a5d147fcfc1f30d60be0b85087d70e
vcokltfre <vcokltfre@gmail.com>
2021-03-08 04:50:16 +0000

    feat: add db interface

---
+++ src/utils/database.py
@@ -0,0 +1,76 @@
+from asyncio import get_event_loop
+from asyncpg import create_pool
+from os import getenv
+from loguru import logger
+from json import dumps
+
+
+class Database:
+    """A database interface for the bot to connect to Postgres."""
+
+    def __init__(self):
+        self.guilds = {}
+        self.users = {}
+
+        loop = get_event_loop()
+        loop.run_until_complete(self.setup())
+
+    async def setup(self):
+        logger.info("Setting up database connections...")
+        self.pool = await create_pool(
+            host=getenv("DB_HOST", "127.0.0.1"),
+            port=getenv("DB_PORT", 5432),
+            database=getenv("DB_DATABASE", "crosschat"),
+            user=getenv("DB_USER", "root"),
+            password=getenv("DB_PASS", "password"),
+        )
+        logger.info("Database setup complete.")
+
+    async def execute(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            await conn.execute(query, *args)
+
+    async def fetchrow(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+
+    async def fetch(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
+
+    # Guild Coros
+    async def get_guild(self, guild_id: int):
+        if guild_id in self.guilds:
+            return self.guilds[guild_id]
+
+        data = await self.fetchrow("SELECT * FROM Guilds WHERE id = $1;", guild_id)
+        self.guilds[guild_id] = data
+        return data
+
+    async def create_guild(self, guild_id: int, config: dict = {}):
+        await self.execute("INSERT INTO Guilds (id, config) VALUES ($1, $2);", guild_id, dumps(config))
+        self.guilds.pop(guild_id, None)  # Shouldn't be needed, but I want to make sure
+
+    async def update_guild(self, guild_id: int, config: dict):
+        await self.execute("UPDATE Guilds SET config = $2 WHERE id = $1;", guild_id, dumps(config))
+        self.guilds.pop(guild_id, None)  # Clear the cache
+
+    async def get_all_guilds(self):
+        return await self.fetch("SELECT * FROM Guilds;")
+
+    # User coros
+    async def get_user(self, user_id: int):
+        if user_id in self.users:
+            return self.users[user_id]
+
+        data = await self.fetchrow("SELECT * FROM Users WHERE id = $1;", user_id)
+        self.users[user_id] = data
+        return data
+
+    async def create_user(self, user_id: int):
+        await self.execute("INSERT INTO Users (id, messages) VALUES ($1, 1);", user_id)
+        self.users.pop(user_id, None)  # Shouldn't be needed, but I want to make sure
+
+    async def update_user_permissions(self, user_id: int, level: int):
+        await self.execute("UPDATE Users SET permissions = $2 WHERE id = $1;", user_id, level)
+        self.users.pop(user_id, None)  # Clear the cache
