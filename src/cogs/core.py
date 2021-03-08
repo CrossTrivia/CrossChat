46a6551c84e275c5bec81e383f9706046a930c16
vcokltfre <vcokltfre@gmail.com>
2021-03-08 05:41:13 +0000

    feat: add message broadcast

--- src/cogs/core.py
+++ src/cogs/core.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Message, TextChannel
 from collections import defaultdict
 from async_rediscache import RedisCache
 from loguru import logger
@@ -28,10 +29,24 @@ class Core(commands.Cog):
             channels = guild.config.get('channels', {})
 
             for local, glob in channels.items():
+                local = int(local)
                 self.channel_mapping[local] = glob
                 self.channels[glob].add(local)
 
         logger.info("Core setup complete.")
+
+    async def _send(self, bcid: int, channel: TextChannel, bypass: bool = False, **kwargs):
+        message = await channel.send(**kwargs)
+
+        await self.bot.db.create_message(message, bcid)
+
+        logger.info(f"Successfully sent message to {channel.id} [BCID: {bcid}]")
+
+    async def broadcast(self, message: Message, channel: str, **kwargs):
+        channels = self.channels.get(channel, [])
+
+        for cid in channels:
+            self.bot.loop.create_task(self._send(message.id, self.bot.get_channel(cid), **kwargs))
 
 
 def setup(bot: Bot):
