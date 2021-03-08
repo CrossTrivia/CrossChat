da3adfe9e5646e756276a6458bab93f23c72eb42
vcokltfre <vcokltfre@gmail.com>
2021-03-08 05:24:16 +0000

    feat: add messages to db

--- static/init.sql
+++ static/init.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS Guilds (
     config          TEXT NOT NULL DEFAULT '{}',
     created_at      TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS Messages (
+    id              BIGINT NOT NULL PRIMARY KEY,
+    bcid            BIGINT NOT NULL,
+    guild_id        BIGINT NOT NULL,
+    channel_id      BIGINT NOT NULL,
+    author_id       BIGINT NOT NULL,
+    content         TEXT NOT NULL,
+    deleted         BOOLEAN NOT NULL DEFAULT FALSE
+);
