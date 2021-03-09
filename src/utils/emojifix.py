e48b0004ac6b9417f6f669dc7788c9db1dc62368
vcokltfre <vcokltfre@gmail.com>
2021-03-09 02:51:30 +0000

    fix: ignore valid emojis while fixing

--- src/utils/emojifix.py
+++ src/utils/emojifix.py
@@ -5,6 +5,7 @@ from src.internal.bot import Bot
 
 EMOJI = compile(r"<a?:\w{2,32}:\d{17,20}>", IGNORECASE)
 NAME = compile(r"[^ :]{2,32}", IGNORECASE)
+ID = compile(r"\d*")
 
 
 class EmojiFixer:
@@ -12,10 +13,16 @@ class EmojiFixer:
         self.bot = bot
 
     def message(self, content: str):
+        botems = {emoji.id for emoji in self.bot.emojis}
+
         ems = []
         for emoji in EMOJI.finditer(content):
             e = emoji.group()
             name = NAME.search(e)
+            _id = ID.search(e).group()
+
+            if _id in botems:
+                continue
 
             for em in self.bot.emojis:
                 em: Emoji
