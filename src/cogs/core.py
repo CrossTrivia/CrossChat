14b79b1bc678825d4cb19097c42366660b77b350
vcokltfre <vcokltfre@gmail.com>
2021-03-19 08:52:28 +0000

    chore: show guild name in embed

--- src/cogs/core.py
+++ src/cogs/core.py
@@ -59,7 +59,7 @@ class Core(commands.Cog):
         )
 
         embed.set_footer(
-            text=str(message.author.id)
+            text=str(message.author.id) + "•" + message.guild.name
         )
 
         return embed
